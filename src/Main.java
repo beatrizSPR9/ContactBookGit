09e9133ce98b1925fddeb70ca7997ccf9fe241a5
Beatriz <b.rosas@campus.fct.unl.pt>
2023-09-22 15:21:15 +0100

    Adicionei comandos ao switch.

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,11 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
-    public static final String QUIT           = "Q";
     public static final String FIND_CONTACT = "GN";
+    public static final String EQUAL_CONTACTS = "EP";
+    public static final String QUIT           = "Q";
+
+
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -23,6 +26,12 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+
+    public static final String PHONE_DOESNT_EXIST = "Phone number does not exist.";
+
+    public static final String PHONES_SHARE_CONTACTS = "There are contacts that share phone numbers.";
+
+    public static final String NO_SHARED_PHONES = "All contacts have different phone numbers";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -53,6 +62,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case FIND_CONTACT:
+                    listContactByNumber(in, cBook);
+                    break;
+                case EQUAL_CONTACTS:
+                    contactsWithSameNumber(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -148,4 +163,13 @@ public class Main {
         }
         else System.out.println(BOOK_EMPTY);
     }
+
+    private static void listContactByNumber(Scanner in, ContactBook cBook) {
+        int number = in.nextInt();
+    }
+
+    private static void contactsWithSameNumber(ContactBook cBook) {
+
+    }
 }
+
